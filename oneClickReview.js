ad32c15a8de2e228fb128df71c373ee6a239fe06
jcdby520@gmail.com <jcdby520@gmail.com>
2018-11-29 22:13:38 +0800

    add review reports mode.

--- oneClickReview.js
+++ oneClickReview.js
@@ -1,16 +1,26 @@
+/**
+ *
+ * 使用方法： node onClickReview --sid <sid> --type <d or w> --command <rr or rb>
+ *     type 是指周或日志， 主要用于批复command rb
+ *     command 可以是rr reviewReport 活着 rb reviewBlogs. 当为rr时， 可以不输入type.
+ */
+
+
+
+
 var request = require("request");
 var commands = new require("./commands")();
 var students = [];
 var jsessionid = "";
 var frequen_reply_to_students = [
   "已阅。祝贺顺利完成毕业实习!",
-  '已阅， 实习辛苦了,祝贺顺利完成毕业实习!'
+  '已阅。实习辛苦了,祝贺顺利完成毕业实习!'
 ];
 // some test
 var args = process.argv;
 var sid = "";
 var blogType = "d";
-var command = "rb"; // review blogs.
+var command = "rb"; // review blogs. this is default command
 console.log(args);
 
 
@@ -26,20 +36,21 @@ commandIndex = commandIndex === -1 ? false : commandIndex + 1;
 
 if (index) {
   sid = args[index];
-  console.log(sid);
+  console.log("user sid is %s", sid);
 }
 
 if (blogTypeIndex) {
   blogType = args[blogTypeIndex];
-  console.log(blogType);
+  console.log("blog type is %s", blogType);
 }
 
 if (commandIndex) {
-
+    command = args[commandIndex];
+    console.log("command is %s", command);
 }
 
 
-function getStudents(sid) {
+function reviewBlogs() {
   var options = {
     method: 'POST',
     url: 'http://www.xybsyw.com/json/practice/school/blogs/loadBlogsBySchool.xhtml',
@@ -66,8 +77,12 @@ function getStudents(sid) {
     console.log(students);
     console.log("students ready!");
     if (students && students.length > 0) {
-      resolve_review(students, sid);
-      console.log("Will review total %d studens. ", students.length);
+        try {
+            resolveReviewBlogs(students, sid);
+        } catch(e) {
+            console.error(e);
+        }
+        console.log("Will review total %d studens. ", students.length);
 
     } else {
       console.log("没有学生可以批阅！");
@@ -76,7 +91,7 @@ function getStudents(sid) {
   });
 }
 
-async function resolve_review(students,sid) {
+async function resolveReviewBlogs(students,sid) {
 
   var num_retry = 10;
   students.forEach(async function(student) {
@@ -92,15 +107,85 @@ async function resolve_review(students,sid) {
         version += 1;
         console.log(" student %s retry %s ", student.name, version);
         options = setOptions(id,sid,version);
-        res_body = await requestToReview(options)
+        res_body = await requestToReview(options);
         isSuccess = JSON.parse(res_body).success;
-      }
 
+      }
       console.log(res_body);
 
-        
-  })
+  });
 }
+
+function reviewReports() {
+    var options = {
+        method: 'POST',
+        url: 'http://www.xybsyw.com/json/practice/school/report/loadReport.xhtml',
+        headers: {
+            'Postman-Token': '853ab258-535d-490d-bbc9-f5151d730a0d',
+            'cache-control': 'no-cache',
+            Cookie: 'JSESSIONID=' + sid + '; Hm_lvt_5b943524066f14e8c8dc6a3c3a69d9ca=1540560756; xybsyw_login_method_type=NORMAL; xybsyw_login_school_id=7822; xybsyw_login_school_name=%E6%B1%9F%E8%8B%8F%E5%A4%A7%E5%AD%A6; xybsyw_login_user_name=1000004788; Hm_lpvt_5b943524066f14e8c8dc6a3c3a69d9ca=1540560819; SERVERID=a400a61a5a6aa2ec013302c9b2706450|1540560848|1540560755',
+            'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        },
+        formData: {
+            on: 'true',
+            page: '1',
+            pageSize: '500',
+            reviewStatus: 'PENDING',
+            schoolTermId: '1159',
+            schoolYear: '2018'
+        }
+    };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            students = JSON.parse(body);
+            students = students && students.datas;
+            console.log(students);
+            console.log("students ready!");
+            if (students && students.length > 0) {
+                try {
+                    resolveReviewReports(students);
+                } catch(e) {
+                    console.error(e)
+                }
+                console.log("Will review total %d students for reports . ", students.length);
+
+            } else {
+                console.log("没有学生的报告可以批阅！");
+
+            }
+        });
+};
+
+
+async function resolveReviewReports (students) {
+    var num_retry = 10;
+    students.forEach(async function(student) {
+        var id = student.id;
+        var version = 0;
+        var options;
+        var success = false;
+
+        options = setOptionsForReports(id);
+        var res_body = await requestToReview(options);
+        var isSuccess = JSON.parse(res_body).success;
+        while(!isSuccess && --num_retry > 0) {
+            version += 1;
+            console.log(" student %s retry %s ", student.id, version);
+            let format = {
+                reviewStatus: 'PASS'
+            }
+            options = setOptionsForReports(id, format);
+            res_body = await requestToReview(options);
+            isSuccess = JSON.parse(res_body).success;
+
+        }
+        console.log(res_body);
+
+    });
+}
+
+
 
 
 function setOptions(id,sid, version) {
@@ -116,8 +201,7 @@ function setOptions(id,sid, version) {
       'Postman-Token': '1020032f-471d-4f57-90e1-38bc7bf3cf39',
       'cache-control': 'no-cache',
       Cookie: 'JSESSIONID='+sid+'; Hm_lvt_5b943524066f14e8c8dc6a3c3a69d9ca=1540560756; xybsyw_login_method_type=NORMAL; xybsyw_login_school_id=7822; xybsyw_login_school_name=%E6%B1%9F%E8%8B%8F%E5%A4%A7%E5%AD%A6; xybsyw_login_user_name=1000004788; Hm_lpvt_5b943524066f14e8c8dc6a3c3a69d9ca=1540562736; SERVERID=a400a61a5a6aa2ec013302c9b2706450|1540562744|1540560755',
-      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
-      referer: "https://www.xybsyw.com/practice/school/blogs/weeklyBlogsReview.xhtml"
+      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
     },
     formData: {
       blogsId: id,
@@ -127,6 +211,37 @@ function setOptions(id,sid, version) {
       needReview: 'true'
     }
   };
+}
+
+function setOptionsForReports(id, format) {
+    let size = frequen_reply_to_students.length;
+    let indexForReplyContent = id % size;
+    let content = frequen_reply_to_students[indexForReplyContent];
+    let formatData;
+    if (!!format) {
+        format.comment = content;
+        format.id = id;
+        formatData = format;
+    } else {
+        formatData = {
+            comment: content,
+            reviewStatus: 'PASS',
+            id: id,
+            score: '5'
+        }
+    }
+
+    return {
+        method: 'POST',
+        url: 'http://www.xybsyw.com/json/practice/school/report/reviewReport.xhtml',
+        headers: {
+            'Postman-Token': '1020032f-471d-4f57-90e1-38bc7bf3cf39',
+            'cache-control': 'no-cache',
+            Cookie: 'JSESSIONID='+sid+'; Hm_lvt_5b943524066f14e8c8dc6a3c3a69d9ca=1540560756; xybsyw_login_method_type=NORMAL; xybsyw_login_school_id=7822; xybsyw_login_school_name=%E6%B1%9F%E8%8B%8F%E5%A4%A7%E5%AD%A6; xybsyw_login_user_name=1000004788; Hm_lpvt_5b943524066f14e8c8dc6a3c3a69d9ca=1540562736; SERVERID=a400a61a5a6aa2ec013302c9b2706450|1540562744|1540560755',
+            'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        },
+        formData: formatData
+    };
 }
 
 function requestToReview(options) {
@@ -141,4 +256,25 @@ function requestToReview(options) {
 }
 
 
-getStudents(sid);
+function start(taskToRun) {
+  taskToRun();
+}
+
+// According to the type to run different task;
+var runTask = reviewBlogs;
+
+switch (command) {
+    case 'rb':
+        runTask = reviewBlogs;
+        break;
+    case 'rr':
+        runTask = reviewReports;
+        break;
+    default:
+        runTask = reviewBlogs;
+        return;
+}
+
+
+
+start(runTask);
